74ef0793f53a787a3ec09295b3eca52eb823fa26
edmondatto <edmond091@gmail.com>
2017-04-19 00:22:54 +0300

    Creates a working find_max_min function

--- Day3/max_min_number.py
+++ Day3/max_min_number.py
@@ -5,4 +5,10 @@ list with the min and max number from the list
 
 
 def find_max_min(num_list):
-    pass
+    if len(set(num_list)) == 1:
+        return [len(num_list)]
+    else:
+        sorted_num_list = sorted(num_list)
+        min_num = sorted_num_list[0]
+        max_num = sorted_num_list[-1]
+        return [min_num, max_num]
