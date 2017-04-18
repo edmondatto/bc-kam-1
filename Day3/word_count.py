0cb6252ae846a6041892ce2ae48b4c7a31bb3a1b
edmondatto <edmond091@gmail.com>
2017-04-18 21:55:05 +0300

    Creates a working words function

--- Day3/word_count.py
+++ Day3/word_count.py
@@ -1,7 +1,17 @@
+from collections import Counter
 """
 A function that counts the occurrences or characters in a word
 """
 
 
 def words(phrase):
-    pass
+    # Check that the argument passed into function is a string
+    if isinstance(phrase, str):
+        # Split the phrase into individual words and add them to a list
+        words_list = phrase.split()
+        # Use Counter from the collections module to count occurrences of words in list
+        # Convert counter object into a dictionary
+        word_occurrences = dict(Counter(words_list))
+        return word_occurrences
+    else:
+        raise TypeError('Argument should be string')
