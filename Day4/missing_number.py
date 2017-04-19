94462f5039fbb401759d2e3efe83a2114030586b
edmondatto <edmond091@gmail.com>
2017-04-19 22:45:05 +0300

    Working missing_number function. Passes all tests

--- Day4/missing_number.py
+++ Day4/missing_number.py
@@ -1,2 +1,12 @@
 def find_missing(num_list_1, num_list_2):
-    pass
+    if len(num_list_1) == 0 and len(num_list_2) == 0:
+        return 0
+    else:
+        num_set_1 = set(num_list_1)
+        num_set_2 = set(num_list_2)
+        missing_number = list(num_set_1.symmetric_difference(num_set_2))
+        print(missing_number)
+        if len(missing_number) == 0:
+            return 0
+        else:
+            return missing_number[0]
